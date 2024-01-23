d3a413f43177060541afb179ff62c6b2b9491bd9
Alise <133121967+chilituna@users.noreply.github.com>
2024-01-23 10:00:14 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,93 +1,31 @@
 
 import random
-
-logo = ''' 
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/    '''
-
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_art import logo
+from hangman_art import stages
+from hangman_words import word_list
 
 #select random word from list
-word_list = ["konttero", "akvaariokala", "lastenvessa", "kissankello", "humma", "kikkoman"]
 chosen_word = random.choice(word_list)
 
-#create a list of blanks for the chosen word and set lives to begin
+#create a list of blanks for the chosen word
 display = []
 for i in range(len(chosen_word)):
   display.append('_')
-lives = 6
 
-#create a list to stored already guessed letters
+#create a list to store already guessed letters
 guessed_letters = []
 
+#set lives and start the game
+lives = 6
 print (logo)
 
+#loop until the word is complete or all lives are gone
+#draw the stage and guessed word
 while '_' in display and lives > 0:
   print(stages[lives])
   print(f"{' '.join(display)}\n")
   correct_answer = False
+  #ask for letter - if included, replace in the displayed word - if not, reduce life - if already guessed, inform user
   guess = input("Guess a letter: ").lower()
   if guess not in guessed_letters:
     guessed_letters.append(guess)
@@ -100,7 +38,8 @@ while '_' in display and lives > 0:
     if correct_answer == False:
       lives -= 1
   else:
-    print("You already gave that word.")
+    print("You already gave that word. Try again")
+#if the displayed word is complete, user wins, otherwise, they lose.
 if '_' not in display:
   print(stages[lives])
   print(f"{' '.join(display)}\n")
